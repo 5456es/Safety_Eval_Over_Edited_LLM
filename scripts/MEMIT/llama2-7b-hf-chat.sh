67f4a2fb2f00e180d6b2d882e062bab083a884ce
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 <1123649556@qq.com>
2024-10-29 22:04:41 +0800

    update cluster scrips

--- scripts/MEMIT/llama2-7b-hf-chat.sh
+++ scripts/MEMIT/llama2-7b-hf-chat.sh
@@ -11,5 +11,5 @@ data_sizes=(1 5 10 15 20 25 30 35 40 45 50)
 
 # 遍历数据大小数组，并将每个数据大小传递给 Python 脚本
 for size in "${data_sizes[@]}"; do
-    python llama2-7b-hf-chat_edit.py --ds_size "$size"
+    python llama2-7b-hf-chat_edit.py --ds_size "$size" --hparams_dir ../../src/hparams/MEMIT/llama2-7b-hf-chat-cluster.yaml
 done
