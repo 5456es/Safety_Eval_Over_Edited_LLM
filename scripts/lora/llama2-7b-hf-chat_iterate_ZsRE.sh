9b1124714d349204e71f28cf3fe68f4eaa217058
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 <1123649556@qq.com>
2024-11-04 21:10:42 +0800

    update for cluster run

--- scripts/lora/llama2-7b-hf-chat_iterate_ZsRE.sh
+++ scripts/lora/llama2-7b-hf-chat_iterate_ZsRE.sh
@@ -1,5 +1,15 @@
+
+
 #!/bin/bash
 
+#SBATCH --job-name=edit_llama_lora      # task name
+#SBATCH --gpus=a100-80:1
+#SBATCH --partition=gpu
+#SBATCH --mem=32G
+#SBATCH --time=0-24:30:00          # 设置作业的最大运行时间为2小时30分钟
+
+# 激活conda环境
+source ~/miniconda3/bin/activate ee  # 使用你安装的conda环境
 
 cd  ../../experiment/Qlora
 
@@ -7,7 +17,7 @@ pwd
 
 data_parts=(0 1 2)
 data_source=ZsRE
-data_sizes=(1)
+data_sizes=(1 5  10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95)
 
 for data_part in "${data_parts[@]}"; do
     for data_size in "${data_sizes[@]}"; do
